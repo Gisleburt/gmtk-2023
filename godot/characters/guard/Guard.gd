adb5a57a021865c8e406b35d20e6ad7565119431
Daniel Mason <daniel@danielmason.com>
2023-07-08 17:42:56 +0100

    smooth out movement and avoid walls

--- godot/characters/guard/Guard.gd
+++ godot/characters/guard/Guard.gd
@@ -7,18 +7,29 @@ const SPEED = 300.0
 
 func _ready() -> void:
 	agent.set_navigation_map(map);
+	agent.velocity_computed.connect(_on_velocity_computed)
+
+func set_target(target: Vector2) -> void:
+	agent.set_target_position(target)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			if event.button_index == MOUSE_BUTTON_LEFT:
-				agent.target_location = get_global_mouse_position()
+				set_target(get_global_mouse_position())
 
 func _physics_process(_delta: float) -> void:
-	if agent.distance_to_target() > agent.target_desired_distance:
-		var dir = agent.get_next_path_position() - position
-		var distance = dir.length();
-		velocity = dir.normalized() * SPEED
-		move_and_slide()
-	else:
-		velocity = Vector2()
+	handle_move()
+
+func _on_velocity_computed(safe_velocity: Vector2):
+	velocity = safe_velocity
+	move_and_slide()
+
+func handle_move():
+	if agent.is_navigation_finished():
+		return
+	
+	var next_pos: Vector2 = agent.get_next_path_position()
+	var current_pos: Vector2 = global_position
+	var new_velocity: Vector2 = (next_pos - current_pos).normalized() * SPEED
+	agent.set_velocity(new_velocity)
