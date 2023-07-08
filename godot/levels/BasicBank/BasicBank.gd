8e4736060407ba6382520afd205c0ce17d8c13a8
Daniel Mason <daniel@danielmason.com>
2023-07-08 22:08:49 +0100

    Move while mouse is held, not just pressed

--- godot/levels/BasicBank/BasicBank.gd
+++ godot/levels/BasicBank/BasicBank.gd
@@ -2,8 +2,15 @@ extends Node2D
 
 @onready var guard: CharacterBody2D = $Guard
 
+var mouse_button_left_held = false;
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if event.is_pressed():
-			if event.button_index == MOUSE_BUTTON_LEFT:
-				guard.set_target(get_global_mouse_position())
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			mouse_button_left_held = event.is_pressed()
+
+
+func _process(delta: float) -> void:
+	if mouse_button_left_held:
+		guard.set_target(get_global_mouse_position())
+
