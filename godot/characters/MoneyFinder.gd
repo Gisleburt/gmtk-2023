f9e7ea037287a416610de187e758c8932d1a0d47
Daniel Mason <daniel@danielmason.com>
2023-07-09 00:22:26 +0100

    Ninja picks up money and moves on

--- godot/characters/MoneyFinder.gd
+++ godot/characters/MoneyFinder.gd
@@ -4,6 +4,12 @@ func _ready() -> void:
 	super()
 	set_next_money()
 
+func _process(_delta: float) -> void:
+	if agent.is_navigation_finished():
+		set_next_money()
+
 func set_next_money() -> void:
 	var money: Array = get_tree().get_nodes_in_group("Money");
-	set_target(money[randi() % money.size()].global_position)
+	if money.size() > 0:
+		set_target(money[randi() % money.size()].global_position)
+
--- godot/items/MoneyPile.gd
+++ godot/items/MoneyPile.gd
@@ -1,3 +1,10 @@
 extends Area2D
 
 @export var value: int = 100
+
+func _ready() -> void:
+	connect("body_entered", _on_body_entered)
+
+func _on_body_entered(node: Node2D) -> void:
+	if node.name == "Ninja":
+		queue_free()
