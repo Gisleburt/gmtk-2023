768d55a55eb38c3d1b34795d8dc29c488ca55594
Daniel Mason <daniel@danielmason.com>
2023-07-09 14:07:09 +0100

    Ninja will head to exit when done

--- godot/characters/MoneyFinder.gd
+++ godot/characters/MoneyFinder.gd
@@ -5,10 +5,12 @@ enum Behaviour { FINDING_MONEY, RUNNING_AWAY, ESCAPING }
 @export var guard: CharacterBody2D
 @onready var guard_detector: Area2D = $GuardDetector
 var current_behaviour: Behaviour = Behaviour.FINDING_MONEY
+var guard_visible: bool = false
 
 func _ready() -> void:
 	super()
 	guard_detector.connect("body_entered", guard_detected)
+	guard_detector.connect("body_exited", guard_lost)
 	set_next_money()
 
 func _process(_delta: float) -> void:
@@ -20,19 +22,30 @@ func set_next_money() -> void:
 	var money: Array = get_tree().get_nodes_in_group("Money");
 	if money.size() > 0:
 		set_target(money[randi() % money.size()].global_position)
+	else:
+		escape()
 
 func run_away() -> void:
-	if current_behaviour != Behaviour.RUNNING_AWAY:
-		current_behaviour = Behaviour.RUNNING_AWAY
+	current_behaviour = Behaviour.RUNNING_AWAY
 
-		var viable_targets = map.get_used_cells(0) \
-			.map(cell_cords) \
-			.filter(is_guard_in_that_direction)
+	var viable_targets = map.get_used_cells(0) \
+		.map(cell_cords) \
+		.filter(is_guard_in_that_direction)
 
-		if viable_targets.size() > 0:
-			viable_targets.sort_custom(distance_to_guard_sort)
-			var index = randi() % roundi(viable_targets.size() * 0.25)
-			set_target(viable_targets[index])
+	if viable_targets.size() > 0:
+		viable_targets.sort_custom(distance_to_guard_sort)
+		var index = randi() % roundi(viable_targets.size() * 0.25)
+		set_target(viable_targets[index])
+
+func escape() -> void:
+	var exit_points = get_tree().get_nodes_in_group("ExitPoint")
+
+	if guard_visible:
+		exit_points.sort_custom(angle_away_from_guard_sort)
+		set_target(exit_points[0])
+		return
+	
+	set_target(exit_points[randi() % exit_points.size()].global_position)
 
 func cell_cords(tile: Vector2i) -> Vector2:
 	return map.to_global(map.map_to_local(tile)) + Vector2(24, 24)
@@ -48,4 +61,19 @@ func distance_squared_to_guard(pos: Vector2) -> float:
 	return guard.global_position.distance_squared_to(pos)
 
 func guard_detected(body: Node2D) -> void:
-	run_away()
+	guard_visible = true
+
+	if current_behaviour == Behaviour.FINDING_MONEY:
+		run_away()
+
+	if current_behaviour == Behaviour.ESCAPING:
+		escape() # look for a different way out
+
+func angle_away_from_guard(pos: Vector2) -> float:
+	return absf(guard.get_angle_to(pos))
+
+func angle_away_from_guard_sort(a: Vector2, b: Vector2) -> bool:
+	return angle_away_from_guard(a) > angle_away_from_guard(b)
+
+func guard_lost() -> void:
+	guard_visible = false
